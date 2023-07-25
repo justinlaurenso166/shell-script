74516278e0425b46212f06f46de147abc7d53f04
Justin Laurenso <justin.laurenso166@gmail.com>
2023-07-25 12:39:42 +0700

    Termux Info Test

--- script.sh
+++ script.sh
@@ -1,8 +1,1 @@
-#!/data/data/com.termux/files/usr/bin/bash
-
-# Fetch Termux information using termux-info and save it to a file
-
-info_file="/data/data/com.termux/files/home/termux_info.txt"
-
-termux-info > "$info_file"
-echo "Termux information fetched successfully."
+termux-info
