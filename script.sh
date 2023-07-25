11b7706c0d2c8d9d95f2fb2782e8280332d0aef9
Justin Laurenso <justin.laurenso166@gmail.com>
2023-07-25 12:32:17 +0700

    Termux Info Test

--- script.sh
+++ script.sh
@@ -1,1 +1,13 @@
-echo "hello world"
+#!/data/data/com.termux/files/usr/bin/bash
+
+# Script to print Termux information
+
+echo "Termux Information:"
+echo "-------------------"
+echo "Home Directory: $HOME"
+echo "User: $USER"
+echo "UID: $UID"
+echo "Architecture: $(dpkg --print-architecture)"
+echo "Android API Level: $(getprop ro.build.version.sdk)"
+echo "Termux Version: $(dpkg --status com.termux | grep Version)"
+echo "-------------------"
