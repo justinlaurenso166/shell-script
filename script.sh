b9a3d838de05e07212f6e65f1561aa6680ca4ab4
Justin Laurenso <justin.laurenso166@gmail.com>
2023-08-07 22:41:44 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,14 @@
-echo "hello world"
+#!/bin/bash
+
+# Start the Alpine distro and execute the commands inside
+proot-distro login alpine <<EOF
+# Commands inside Alpine distro
+apk update && apk add --no-cache nmap && \
+  echo @edge http://nl.alpinelinux.org/alpine/edge/community >> /etc/apk/repositories && \
+  echo @edge http://nl.alpinelinux.org/alpine/edge/main >> /etc/apk/repositories && \
+  apk update && \
+  apk add --no-cache chromium && apk add git && apk add nodejs
+  git clone https://github.com/Alien-404/yt-automate.git
+  cd yt-automate
+  ls
+EOF
