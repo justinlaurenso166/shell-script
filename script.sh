17661561d07973afc105dddc3a8c05481ea15838
Justin Laurenso <justin.laurenso166@gmail.com>
2023-08-07 23:31:48 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,9 +7,15 @@ apk update && apk add --no-cache nmap && \
   echo @edge http://nl.alpinelinux.org/alpine/edge/community >> /etc/apk/repositories && \
   echo @edge http://nl.alpinelinux.org/alpine/edge/main >> /etc/apk/repositories && \
   apk update && \
-  apk add --no-cache chromium && apk add git && apk add nodejs && apk add npm
-  git clone https://github.com/Alien-404/yt-automate.git
-  cd yt-automate
-  npm install
-  npm run setup
+  apk add --no-cache chromium && apk add git && apk add nodejs && apk add npm \
+  git clone https://github.com/Alien-404/yt-automate.git \
+  cd yt-automate \ 
+  npm install \
+  npm run setup &
 EOF
+
+# Wait for the background process to complete
+wait
+
+# Continue with the rest of the script
+echo "All commands have been executed."
