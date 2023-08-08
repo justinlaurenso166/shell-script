1354fe3603a28df19512fc1493652c32e86e9cea
Justin Laurenso <justin.laurenso166@gmail.com>
2023-08-08 08:32:11 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,10 +7,14 @@ apk update && apk add --no-cache nmap && \
   echo @edge http://nl.alpinelinux.org/alpine/edge/community >> /etc/apk/repositories && \
   echo @edge http://nl.alpinelinux.org/alpine/edge/main >> /etc/apk/repositories && \
   apk update && \
-  apk add --no-cache chromium && apk add git && apk add nodejs && apk add npm
-  git clone https://github.com/Alien-404/yt-automate.git
-  cd yt-automate
-  npm install
+  apk add --no-cache chromium && apk add git && apk add nodejs && apk add npm && \
+  git clone https://github.com/Alien-404/yt-automate.git  && \
+  cd yt-automate && \
+  npm install && \
+  read -p "Masukkan email Facebook Anda : " email && \
+  read -p "Masukkan password Facebook Anda : " password && \
+  read -p "Masukkan teks komentar Facebook Anda : " comment && \
+  read -p "Masukkan URL grup tujuan : " group_url && \
+  node ./config/single -e "$email" -p "$password" -c "$comment" -g "$group_url" && \
+  node index.js
 EOF
-
-npm run setup
